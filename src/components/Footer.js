2038b9706a21c9ebd99ab7ed0aeadf89e7cd905b
jacksonnorris <jacksonnorr@gmail.com>
2022-04-28 10:21:17 -0400

    updated sizing on portfolio page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 const Footer = () => {
   return (
     <div className='footerWrapper'>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -12,10 +12,12 @@ export default function Portfolio() {
   return (
     <div>
       <h1>My Portfolio</h1>
-      <div className="contentWrapper">
-        <Project url={urls[0]} name={names[0]} id={ids[0]} src={srcs[0]} />
-        <Project url={urls[1]} name={names[1]} id={ids[1]} src={srcs[1]} />
-      </div>
+      <section>
+        <div className="contentWrapper">
+          <Project url={urls[0]} name={names[0]} id={ids[0]} src={srcs[0]} />
+          <Project url={urls[1]} name={names[1]} id={ids[1]} src={srcs[1]} />
+        </div>
+      </section>
     </div>
   );
 }
