786fef6ef696cb4202e3a3345b2994fea9b54559
jacksonnorris <jacksonnorr@gmail.com>
2022-05-01 18:23:17 -0400

    added profile picture to about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import profilePicture from '../assets/profilePic.png';
 
 export default function About() {
   return (
     <div>
       <h1>About Me</h1>
+      <img src={profilePicture} alt='profile' className="aboutMeImg"></img>
       <p>
         Hi, my name is Jackson Norris. I am a front-end web developer with undergraduate Computer Science at UNC Chapel Hill and am working towards a Certificate in Full-stack development. Skilled with JavaScript, HTML, and CSS and have experience in Java, C, Python, and SQL. I am passionate about solving challenging problems and approaching new material to further develop my abilities. I’ve successfully developed and maintained eCommerce websites run on Netsuite, WordPress, and Shopify, and also have past experience in marketing and working with graphic designers. I enjoy building powerful, yet simple tools that look to accomplish as much as possible while using few resources. I do enjoy working with companies involved in eCommerce and working on content that users get to see and interact with.
       </p>
