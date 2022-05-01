5fb6b0dd9100289849143ff5119c96ce40339880
jacksonnorris <jacksonnorr@gmail.com>
2022-05-01 16:33:07 -0400

    updated resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import resumePdf from '../assets/Jackson Norris Resume 3-15-22.pdf';
 
 export default function Resume() {
   return (
     <div>
       <h1>My Resume</h1>
       <p>
-
+        <a href={resumePdf} >Download my resume</a>
       </p>
+      <h3>Skills</h3>
+      <ul>
+        <li>JavaScript</li>
+        <li>Java</li>
+        <li>SQL and NoSQL</li>
+        <li>Python</li>
+        <li>C</li>
+        <li>MongoDb and Mongoose</li>
+        <li>Node</li>
+        <li>React</li>
+      </ul>
     </div>
   );
 }
