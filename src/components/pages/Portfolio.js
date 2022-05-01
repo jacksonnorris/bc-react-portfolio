f447aafaafae61e44a1ed4cb64557ab9d0a443d2
jacksonnorris <jacksonnorr@gmail.com>
2022-05-01 16:18:20 -0400

    added more projects to portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Project from '../Project';
 import noteTakerImg from '../assets/noteTaker.png';
 import thirdStImg from '../assets/3rdStreetInn.png';
+import cryptoImg from '../assets/cryptoViewer.png';
+import carImg from '../assets/car-quiz.png';
 
-const urls = ['https://github.com/jacksonnorris/bc-note-taker', 'https://3rdstreetinn.com/'];
-const names = ['Note Taking App', 'Shopify Site'];
-const ids = ['noteTaker', '3stInn'];
-const srcs = [noteTakerImg, thirdStImg];
+const urls = ['https://github.com/jacksonnorris/bc-crypto-viewer', 'https://github.com/jacksonnorris/bc-note-taker', 'https://github.com/jacksonnorris/bc-quiz-app', 'https://3rdstreetinn.com/'];
+const names = ['Crypto Price Viewer', 'Note Taking App', 'Javascript Car Quiz', 'Shopify Site'];
+const ids = ['cryptoViewer', 'noteTaker', 'carQuiz', '3stInn'];
+const srcs = [cryptoImg, noteTakerImg, carImg, thirdStImg];
 
 export default function Portfolio() {
   return (
@@ -16,6 +18,8 @@ export default function Portfolio() {
         <div className="contentWrapper">
           <Project url={urls[0]} name={names[0]} id={ids[0]} src={srcs[0]} />
           <Project url={urls[1]} name={names[1]} id={ids[1]} src={srcs[1]} />
+          <Project url={urls[2]} name={names[2]} id={ids[2]} src={srcs[2]} />
+          <Project url={urls[3]} name={names[3]} id={ids[3]} src={srcs[3]} />
         </div>
       </section>
     </div>
