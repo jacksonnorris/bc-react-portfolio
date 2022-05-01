0cb4e26be577ef492c5af9135239751e39d35ad3
jacksonnorris <jacksonnorr@gmail.com>
2022-05-01 16:47:08 -0400

    updated contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,16 +5,16 @@ export default function Contact() {
     <div>
       <h1>Contact Me!</h1>
       <form>
-        <div class="form-group">
-          <label for="name">Name</label>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
           <input type="text" className="form-control" id="name" placeholder="Name" />
         </div>
-        <div class="form-group">
-          <label for="email">Email address</label>
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
           <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
         </div>
-        <div class="form-group">
-          <label for="message">Message</label>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
           <textarea className="form-control" id="message" placeholder="Enter message" rows="6" />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
